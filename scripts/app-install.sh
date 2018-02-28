682319b0153fcff961dd61e1d65b4fab9b5050fe
Josef Zavisek <josef.zavisek@strv.com>
2018-02-28 22:05:30 +0100

    feat: Test install

--- scripts/app-install.sh
+++ scripts/app-install.sh
@@ -4,6 +4,11 @@
 cd /home/ec2-user/test-server
 chmod -R +x scripts/
 
+# install node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 9.6.1
+
 # install dependencies
 npm install -g forever
 npm install
