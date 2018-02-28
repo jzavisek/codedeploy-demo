9c944c35136f0efa7d5e7558d9a4f76ff4865fb0
Josef Zavisek <josef.zavisek@strv.com>
2018-02-28 22:41:56 +0100

    feat: Code deploy tests

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -7,4 +7,4 @@ cd /home/ec2-user/test-server
 npm install
 
 # starting
-PORT=80 forever start -a -l forever.log -o console.log -e error.log src/server.js
+sudo PORT=80 forever start -a -l forever.log -o console.log -e error.log src/server.js
