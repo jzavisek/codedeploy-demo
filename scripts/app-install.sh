82a4943e56766318093d6ff0fe99c037deaefa63
Josef Zavisek <josef.zavisek@strv.com>
2018-02-28 22:44:07 +0100

    feat: try as regular user

--- scripts/app-install.sh
+++ scripts/app-install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# set execute permissions to scripts folder
-# cd /home/ec2-user/test-server
-# chmod -R +x scripts/
-
 # install node
 curl -sL https://rpm.nodesource.com/setup_9.x | bash -
-sudo yum install -y nodejs
+yum install -y nodejs
 
 # install dependencies
-sudo npm install -g forever
+npm install -g forever
